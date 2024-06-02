0bc47f2c26384c5be71f2a0fb967c4cc32610420
Daniel <dani3lkamanthi@gmail.com>
2024-06-02 20:58:12 +0300

    Completes code challenge

--- lib/classes/many_to_many.py
+++ lib/classes/many_to_many.py
@@ -1,8 +1,11 @@
 class Article:
+    all = []
     def __init__(self, author, magazine, title):
         self.author = author
         self.magazine = magazine
         self.title = title
+        Article.all.append(self)
+        # self.author.add_article(self)
     
     @property
     def title(self):
@@ -17,10 +20,33 @@ class Article:
         if hasattr(self, '_title'):
             raise AttributeError("Title cannot be changed after it has been set.")
         self._title = title
-        
+
+    @property
+    def author(self):
+        return self._author
+    
+    @author.setter
+    def author(self, author):
+        if not isinstance(author, Author):
+            raise TypeError('Author must be an instance of the Author class.')
+        self._author = author
+
+    @property
+    def magazine(self):
+        return self._magazine
+    
+    @magazine.setter
+    def magazine(self, magazine):
+        if not isinstance(magazine, Magazine):
+            raise TypeError('Magazine must be an instance of the Magazine class.')
+        self._magazine = magazine
+
 class Author:
     def __init__(self, name):
         self.name = name
+        self._articles = []
+        self._magazines = set()
+
 
     @property
     def name(self):
@@ -37,21 +63,28 @@ class Author:
         self._name = name
 
     def articles(self):
-        pass
+        return self._articles
 
     def magazines(self):
-        pass
+        return list(self._magazines)
 
     def add_article(self, magazine, title):
-        pass
+        article = Article(self, magazine, title)
+        self._articles.append(article)
+        self._magazines.add(magazine)
+        return article
 
     def topic_areas(self):
-        pass
+        if not self._articles:
+            return None
+        return list(set(article.magazine.category for article in self._articles))
 
 class Magazine:
     def __init__(self, name, category):
         self.name = name
         self.category = category
+        self._articles = []
+        self._contributors = set()
 
     @property
     def name(self):
@@ -78,13 +111,43 @@ class Magazine:
         self._category = category
 
     def articles(self):
-        pass
+        return self._articles
 
     def contributors(self):
-        pass
+        return list(self._contributors)
 
     def article_titles(self):
-        pass
+        if self._articles:
+            return [article.title for article in self._articles]
+        else:
+            return None
 
     def contributing_authors(self):
-        pass
+        author_counts = {}
+        for article in self._articles:
+            author = article.author
+            if author in author_counts:
+                author_counts[author] += 1
+            else:
+                author_counts[author] = 1
+        
+        contributing_authors = [author for author, count in author_counts.items() if count > 2]
+        if contributing_authors:
+            return contributing_authors
+        else: 
+            return None
+        
+    @classmethod
+    def top_publisher(cls):
+        if not cls.all:
+            return None
+        magazine_counts = {}
+        for magazine in cls.all:
+            if magazine in magazine_counts:
+                magazine_counts[magazine] += 1
+            else:
+                magazine_counts[magazine] = 1
+        top_magazine = max(magazine_counts, key=magazine_counts.get)
+        return top_magazine
+    
+
